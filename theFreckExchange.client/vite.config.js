f5d6a87ba7aab447f49d2de8fed63015e95c13dd
theFreck <wpicaco@gmail.com>
2024-11-16 16:46:34 -0700

    Cheese and Vite'n Rice!

--- theFreckExchange.client/vite.config.js
+++ theFreckExchange.client/vite.config.js
@@ -7,15 +7,20 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASPNET/https`
-        : `${env.HOME}/.aspnet/https`;
 
+const baseFolder =
+env.APPDATA !== undefined && env.APPDATA !== ''
+? `${env.APPDATA}/ASPNET/https`
+: `${env.HOME}/.aspnet/https`;
+
+fs.mkdirSync(baseFolder, {recursive: true});
 
 const certificateName = "theFreckExchange.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+console.log("certificateName: ", certificateName);
+console.log("certFilePath: ", certFilePath);
+console.log("keyFilePath: ", keyFilePath);
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     if (0 !== child_process.spawnSync('dotnet', [
@@ -34,6 +39,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7299';
 
+console.log("target: ", target);
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
