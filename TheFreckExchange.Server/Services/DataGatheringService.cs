5eac23484a9a49674c28fabfd4487ce40c531e6c
theFreck <wpicaco@gmail.com>
2024-11-18 14:24:22 -0700

    switched from HtmlAgility to AngleSharp

--- TheFreckExchange.Server/Services/DataGatheringService.cs
+++ TheFreckExchange.Server/Services/DataGatheringService.cs
@@ -1,6 +1,7 @@
-﻿using HtmlAgilityPack;
+﻿//using HtmlAgilityPack;
 using TheFreckExchange.Server.DTO;
 using System.Text;
+using AngleSharp;
 
 namespace TheFreckExchange.Server.Services
 {
@@ -11,26 +12,42 @@ namespace TheFreckExchange.Server.Services
 
     public class DataGatheringService : IDataGatheringService
     {
-        private readonly HtmlWeb web;
+        //private readonly HtmlWeb web;
+        private readonly AngleSharp.IConfiguration config;
 
         public DataGatheringService()
         {
-            web = new HtmlWeb();
+            config = Configuration.Default;
+            //web = new HtmlWeb();
         }
 
         public async Task<string> GetDataAsync(string hatname)
         {
             var url = DescriptionUrls.GetUrl(hatname);
-            var site = await web.LoadFromWebAsync(url);
-            var ps = site.DocumentNode.QuerySelectorAll("p");
-            var stringified = new StringBuilder();
-            foreach (var p in ps) 
+
+            using (HttpClient client = new HttpClient()) 
             {
-                
-                stringified.AppendLine(p.OuterHtml);
+                HttpResponseMessage response = await client.GetAsync(url);
+                string htmlContent = await response.Content.ReadAsStringAsync();
+
+                var config = Configuration.Default.WithDefaultLoader();
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(req => req.Content(htmlContent));
+                var ps = document.QuerySelectorAll("p");
+
+                var stringified = new StringBuilder();
+                foreach (var p in ps)
+                {
+
+                    stringified.AppendLine(p.OuterHtml);
+                }
+                var text = stringified.ToString();
+                return text;
             }
-            var text = stringified.ToString();
-            return text;
+
+            //var site = await web.LoadFromWebAsync(url);
+            //var ps = site.DocumentNode.QuerySelectorAll("p");
+
         }
     }
 }
