093730c08f99167b8c1be48a24c8bd7e5f95ee06
theFreck <wpicaco@gmail.com>
2024-11-21 12:48:08 -0700

    hold my beer

--- theFreckExchange.client/vite.config.js
+++ theFreckExchange.client/vite.config.js
@@ -36,7 +36,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7299';
 
-console.log("import.meta.url: ", import.meta.url);
+console.log("target: ", target);
 
 // https://vitejs.dev/config/
 export default defineConfig({
