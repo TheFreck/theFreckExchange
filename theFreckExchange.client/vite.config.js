793af977dd6c343e6723eabb855536fd82747974
theFreck <wpicaco@gmail.com>
2024-11-21 12:30:18 -0700

    testing sumpin

--- theFreckExchange.client/vite.config.js
+++ theFreckExchange.client/vite.config.js
@@ -36,6 +36,8 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7299';
 
+console.log("import.meta.url: ", import.meta.url);
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
